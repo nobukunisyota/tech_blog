b62333d7559c589105fa11c8211ce9d989e194d8
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-12-06 23:34:16 +0900

    update StaffModel

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe StaffMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "password=" do
+    example "入力された文字列に対して、hashed_passwordは長さ60の文字列になる" do
+      member = StaffMember.new
+      member.password = "test_password"
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    example "入力された文字列がnilの時、hashed_passwordはnilになる" do
+      member = StaffMember.new(hashed_password="test")
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
