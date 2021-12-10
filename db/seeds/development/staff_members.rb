deb628a7cbb9e3c2d1d599141e6b524c98362fbd
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-12-10 23:27:42 +0900

    add seed data in StaffMember DataBase

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,10 @@
+StaffMember.create!(
+    email: "nobukuni.syota@hoge.com",
+    family_name: "保下保下",
+    given_name: "歩画歩画",
+    family_name_kana: "ホゲホゲ",
+    given_name_kana: "フガフガ",
+    password: "password",
+    start_data: Date.today,
+    suspended: false
+)
