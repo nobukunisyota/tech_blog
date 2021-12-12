c30e1dccbf6352bdfec4c7625b333d6d17e8cf0d
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-12-12 23:16:40 +0900

    update login feature

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,4 +1,4 @@
-class Staff::Base < AplicationContoller
+class Staff::Base < ApplicationController
     private def current_staff_menber
         if session[:staff_member_id]
             @current_staff_menber ||= StaffMember.find_by(id: session[:staff_member_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :staff do
     root 'top#index'
+    get "login" => "session#new", as: :login
+    post "session" => "session#create", as: :session
+    delete "session" => "session#destroy"
   end
   namespace :admin do
     root 'top#index'
