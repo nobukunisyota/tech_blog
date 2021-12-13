d34591c169216b481fdb2b18852eb2856c480e6f
nobukunisyota <nobukuni.syota0727@gmail.com>
2021-12-13 22:20:42 +0900

    bugfix : method name is incorrect change _header.html.erb directory revel

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,9 +1,9 @@
 class Staff::Base < ApplicationController
-    private def current_staff_menber
+    private def current_staff_member
         if session[:staff_member_id]
             @current_staff_menber ||= StaffMember.find_by(id: session[:staff_member_id])
         end
     end
 
-    helper_method :current_staff_menber
+    helper_method :current_staff_member
 end
